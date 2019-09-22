f8b7e2ca685ed8ab3a6dcb2c61f913ff56488dcc
fltd1998 <55661928+fltd1998@users.noreply.github.com>
2019-09-23 01:28:42 +0800

    Update mydataset.js

--- mydataset.js
+++ mydataset.js
@@ -1,20 +1,36 @@
 var mydataset = {
 	digitalartpage: [{
+		link: "https://i.pinimg.com/originals/2a/ca/5f/2aca5f4c7c8729542aa36f2772a4fb1d.jpg",
+		title: "You See See You",
+		descrip: "Sep 2019"
+	},{
+		link: "https://i.pinimg.com/originals/33/2f/3d/332f3de81302a2808e231eee8eb5fda4.jpg",
+		title: "Helpless/Hopeless/What",
+		descrip: "Sep 2019"
+	},{
 		link: "https://i.pinimg.com/originals/7e/6a/de/7e6ade908566a992c6e3ef2ba66c5f58.jpg",
-		title: "title",
-		descrip: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque."
+		title: "前 Forward",
+		descrip: "Sep 2019"
 	},{
-		link: "https://i.pinimg.com/originals/33/2f/3d/332f3de81302a2808e231eee8eb5fda4.jpg",
-		title: "",
-		descrip: ""
+		link: "https://i.pinimg.com/originals/74/ff/55/74ff556893a24a4d636f29e0ae48aa3c.jpg",
+		title: "美好生活 Life",
+		descrip: "July 2019"
 	},{
-		link: "https://i.pinimg.com/originals/33/2f/3d/332f3de81302a2808e231eee8eb5fda4.jpg",
-		title: "asd",
-		descrip: "asdfasdfasdfasdf"
+		link: "https://i.pinimg.com/originals/af/1f/0c/af1f0cb23e5279e2e21e579a445249ad.jpg",
+		title: "The Pursuit Of",
+		descrip: "July 2019"
 	},{
-		link: "https://i.pinimg.com/originals/33/2f/3d/332f3de81302a2808e231eee8eb5fda4.jpg",
-		title: "asd",
-		descrip: "asdfasdfasdfasdf"
+		link: "https://i.pinimg.com/originals/22/0b/16/220b167c793ad98b9d7e77b8531637b3.jpg",
+		title: "自由 Freedom",
+		descrip: "June 2019"
+	},{
+		link: "https://i.pinimg.com/originals/a2/fa/21/a2fa21f03aa89ba862717739e345cc47.jpg",
+		title: "在滿洲里等你 Sitting Still",
+		descrip: "June 2019"
+	},{
+		link: "https://i.pinimg.com/originals/c3/d8/f1/c3d8f16e390c64c21e4673979880e6a2.jpg",
+		title: "Hi Boss",
+		descrip: "May 2019"
 	},{
 		link: "https://i.pinimg.com/originals/33/2f/3d/332f3de81302a2808e231eee8eb5fda4.jpg",
 		title: "asd",
