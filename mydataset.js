b6b10539c748824ac161ee8cc78b1a3ae1cae89a
harrykwan <33515754+harrykwan@users.noreply.github.com>
2019-09-22 23:05:40 +0800

    Add files via upload

---
+++ mydataset.js
@@ -0,0 +1,41 @@
+var mydataset = {
+	digitalartpage: [{
+		link: "https://i.pinimg.com/originals/7e/6a/de/7e6ade908566a992c6e3ef2ba66c5f58.jpg",
+		title: "title",
+		descrip: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque."
+	},{
+		link: "https://i.pinimg.com/originals/33/2f/3d/332f3de81302a2808e231eee8eb5fda4.jpg",
+		title: "",
+		descrip: ""
+	},{
+		link: "https://i.pinimg.com/originals/33/2f/3d/332f3de81302a2808e231eee8eb5fda4.jpg",
+		title: "asd",
+		descrip: "asdfasdfasdfasdf"
+	}],
+
+	sketchpage: [{
+		link: "https://i.pinimg.com/originals/7e/6a/de/7e6ade908566a992c6e3ef2ba66c5f58.jpg",
+		title: "title",
+		descrip: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque."
+	},{
+		link: "https://i.pinimg.com/originals/33/2f/3d/332f3de81302a2808e231eee8eb5fda4.jpg",
+		title: "",
+		descrip: ""
+	}],
+
+	paintingpage: [{
+		link: "https://i.pinimg.com/originals/7e/6a/de/7e6ade908566a992c6e3ef2ba66c5f58.jpg",
+		title: "title",
+		descrip: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque."
+	},{
+		link: "https://i.pinimg.com/originals/33/2f/3d/332f3de81302a2808e231eee8eb5fda4.jpg",
+		title: "",
+		descrip: ""
+	}],
+
+	watercolourpage: [{
+		link: "https://i.pinimg.com/originals/7e/6a/de/7e6ade908566a992c6e3ef2ba66c5f58.jpg",
+		title: "title",
+		descrip: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque."
+	}]
+}
